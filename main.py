d2cefc6acd6749c80d36b1fd7a1ac6eecc187604
Francisco <fr.quijadap@duocuc.cl>
2026-05-28 21:57:12 -0400

    Refactor CRUD operations to use funciones module

--- main.py
+++ main.py
@@ -1,64 +1,46 @@
-"""
-Actividad DUOC UC - Debugging con Python
-Archivo principal: main.py
+import funciones as u
 
-IMPORTANTE:
-Este programa contiene errores intencionales.
-La misión del estudiante es clonar el repositorio, crear una rama,
-corregir los errores y dejar funcionando el CRUD.
-"""
+estudiantes = [
 
-from funciones import (
-    agregar_estudiante,
-    listar_estudiantes,
-    buscar_estudiante,
-    actualizar_estudiante,
-    eliminar_estudiante
-)
+]
+print("===== SISTEMA CRUD ESTUDIANTES DUOC UC =====")
+print("1. Agregar estudiante")
+print("2. Listar estudiantes")
+print("3. Buscar estudiante")
+print("4. Actualizar estudiante")
+print("5. Eliminar estudiante")
+print("6. Salir")
 
-estudiantes = []
-
-def mostrar_menu():
-    print("\n===== SISTEMA CRUD ESTUDIANTES DUOC UC =====")
-    print("1. Agregar estudiante")
-    print("2. Listar estudiantes")
-    print("3. Buscar estudiante")
-    print("4. Actualizar estudiante")
-    print("5. Eliminar estudiante")
-    print("6. Salir")
-
-opcion = "0"
+opcion = 0
 
 while opcion != 6:
-    mostrar_menu()
 
     try:
-        opcion = input("Seleccione una opción: ")
+        opcion = int(input("Seleccione una opción: "))
     except:
         print("Error al ingresar la opción")
 
-    if opcion == "1":
-        agregar_estudiante(estudiantes)
+    if opcion == 1:
+        estudiante = u.agregar_estudiante()
+        estudiantes.append(estudiante)
 
-    elif opcion == "2":
-        listar_estudiantes(estudiantes)
+    elif opcion == 2:
+        u.listar_estudiantes(estudiantes)
 
-    elif opcion == "3":
+    elif opcion == 3:
         rut = input("Ingrese RUT del estudiante a buscar: ")
-        buscar_estudiante(estudiantes, rut)
+        u.buscar_estudiante( estudiantes, rut)
 
-    elif opcion == "4":
+    elif opcion == 4:
         rut = input("Ingrese RUT del estudiante a actualizar: ")
-        actualizar_estudiante(estudiantes, rut)
+        u.actualizar_estudiante(estudiantes, rut)
 
-    elif opcion == "5":
+    elif opcion == 5:
         rut = input("Ingrese RUT del estudiante a eliminar: ")
-        eliminar_estudiante(rut, estudiantes)
+        u.eliminar_estudiante(estudiantes, rut)
 
-    elif opcion == "6":
+    elif opcion == 6:
         print("Saliendo del sistema...")
-        break
 
     else:
-        
         print("Opción inválida")
