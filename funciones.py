85afca0a184528d723fc13365b4b149a42e1130f
Francisco <fr.quijadap@duocuc.cl>
2026-05-28 21:57:53 -0400

    Implement CRUD functions for student management

--- funciones.py
+++ funciones.py
@@ -1,46 +1,71 @@
-import funciones as u
+def agregar_estudiante():
+    print       ("--- Agregar estudiante ---")
+    rut         = input("Ingrese RUT: ")
+    nombre      = input("Ingrese nombre: ")
+    carrera     = input("Ingrrese carrera: ")
+    edad        = int(input("Ingrese edad: "))
+    
+    estudiante = {"rut":rut, "nombre": nombre, "carrera": carrera,"edad": edad}
+    print("Estudiante agregado correctamente")
+    
+    return estudiante
+         
 
-estudiantes = [
 
-]
-print("===== SISTEMA CRUD ESTUDIANTES DUOC UC =====")
-print("1. Agregar estudiante")
-print("2. Listar estudiantes")
-print("3. Buscar estudiante")
-print("4. Actualizar estudiante")
-print("5. Eliminar estudiante")
-print("6. Salir")
-
-opcion = 0
-
-while opcion != 6:
-
-    try:
-        opcion = int(input("Seleccione una opción: "))
-    except:
-        print("Error al ingresar la opción")
-
-    if opcion == 1:
-        estudiante = u.agregar_estudiante()
-        estudiantes.append(estudiante)
-
-    elif opcion == 2:
-        u.listar_estudiantes(estudiantes)
-
-    elif opcion == 3:
-        rut = input("Ingrese RUT del estudiante a buscar: ")
-        u.buscar_estudiante( estudiantes, rut)
-
-    elif opcion == 4:
-        rut = input("Ingrese RUT del estudiante a actualizar: ")
-        u.actualizar_estudiante(estudiantes, rut)
-
-    elif opcion == 5:
-        rut = input("Ingrese RUT del estudiante a eliminar: ")
-        u.eliminar_estudiante(estudiantes, rut)
-
-    elif opcion == 6:
-        print("Saliendo del sistema...")
-
+def listar_estudiantes(estudiantes):
+    
+    if len(estudiantes) == 0:
+        print("la lista no tiene estudiantes registrados")
     else:
-        print("Opción inválida")
+        print("\n--- Lista de estudiantes ---")
+        for estudiante in estudiantes:
+            print(estudiante)
+
+
+def buscar_estudiante(estudiantes, rut):
+    print("\n--- Buscar estudiante ---")
+
+    encontrado = False
+
+    for estudiante in estudiantes:
+        if estudiante["rut"] == rut:
+            print("Estudiante encontrado")
+            print(f"RUT: {estudiante['rut']}")
+            print(f"Nombre: {estudiante['nombre']}")
+            print(f"Carrera: {estudiante['carrera']}")
+            print(f"Edad: {estudiante['edad']}")
+            encontrado = True
+
+        if encontrado == False:
+            print("No se encontró el estudiante")
+
+
+def actualizar_estudiante(estudiantes, rut):
+    print("--- Actualizar estudiante ---")
+
+    for estudiante in estudiantes:
+        if estudiante["rut"] == rut:
+            nuevo_nombre = input("Ingrese nuevo nombre: ")
+            nueva_carrera = input("Ingrese nueva carrera: ")
+            nueva_edad = input("Ingrese nueva edad: ")
+
+            estudiante["nombre"] = nuevo_nombre
+            estudiante["carrera"] = nueva_carrera
+            estudiante["edad"] == nueva_edad
+            print("Estudiante actualizado correctamente")
+            return
+    print("No se encontró el estudiante")
+            
+
+
+
+def eliminar_estudiante(estudiantes, rut):
+    print("--- Eliminar estudiante ---")
+
+    for estudiante in estudiantes:
+        if estudiante["rut"] == rut:
+            estudiantes.remove(estudiante)
+            print("Estudiante eliminado correctamente")
+            return
+        
+    print("No se encontró el estudiante")
