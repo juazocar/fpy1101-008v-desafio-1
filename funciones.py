b9012dfc05917b2d13e18be508553c7a4ec27013
Francisco <fr.quijadap@duocuc.cl>
2026-06-02 14:44:23 -0400

    Enhance student addition and deletion with validation
    Refactor agregar_estudiante function to include input validation for RUT, nombre, carrera, and edad. Update eliminar_estudiante function to confirm deletion with user.

--- funciones.py
+++ funciones.py
@@ -1,15 +1,51 @@
 def agregar_estudiante():
-    print       ("--- Agregar estudiante ---")
-    rut         = input("Ingrese RUT: ")
-    nombre      = input("Ingrese nombre: ")
-    carrera     = input("Ingrrese carrera: ")
-    edad        = int(input("Ingrese edad: "))
     
+    print       ("--- Agregar estudiante ---")
+
+    while True:
+        rut = input("Ingrese RUT: ")
+        if rut == "":
+            print("El RUT no puede estar vacío")
+        elif len(rut) != 9:
+            print("El RUT debe tener 8 dígitos")
+        else:
+            break
+
+    while True:
+        nombre = input("Ingrese nombre: ")
+        if nombre == "":
+            print("El nombre no puede estar vacío")
+        else:
+            break
+
+    while True:
+        carrera = input("Ingrese carrera: ")
+        if carrera == "":
+            print("Tienes que ingresar una carrera")
+        else:
+            break
+
+    while True:
+        edad = input("Ingrese edad: ")
+        if edad == "":
+            print("Tienes que ingresar una edad")
+        elif not edad.isdigit():
+            print("La edad debe ser un número")
+        else:
+            edad = int(edad)
+            if edad < 0:
+                print("La edad no puede ser negativa")
+            elif edad > 99:
+                print("La edad no puede ser mayor a 99")
+
+            else:
+                break
+
     estudiante = {"rut":rut, "nombre": nombre, "carrera": carrera,"edad": edad}
     print("Estudiante agregado correctamente")
     
     return estudiante
-         
+
 
 
 def listar_estudiantes(estudiantes):
@@ -61,11 +97,15 @@ def actualizar_estudiante(estudiantes, rut):
 
 def eliminar_estudiante(estudiantes, rut):
     print("--- Eliminar estudiante ---")
-
+    sn=input("¿Está seguro que desea eliminar el estudiante? (s/n): ")
+    if sn.lower() != "s":
+        print("Operación cancelada")
+        return
     for estudiante in estudiantes:
         if estudiante["rut"] == rut:
             estudiantes.remove(estudiante)
             print("Estudiante eliminado correctamente")
             return
-        
+            
     print("No se encontró el estudiante")
+
